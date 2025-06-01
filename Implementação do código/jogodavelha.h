d5a500222126d3617cc51db2b5d9cdba8440bcb5
Yan dos Santos Teixeira <yan.santos12t@gmail.com>
2025-06-01 19:25:10 -0400

    jogodavelha.h

--- Implementação do código/jogodavelha.h
+++ Implementação do código/jogodavelha.h
@@ -8,9 +8,9 @@ typedef struct {
     bool game_over;
 } GameBoard;
 
-GameBoard* create_board();
-void free_board(GameBoard* board);
-void display_board(const GameBoard* board);
+GameBoard* create_board();  // Função para criar o jogo de tabuleiro
+void free_board(GameBoard* board); //Liberar a memória apos o fim da execução do código 
+void display_board(const GameBoard* board); //Essa função cria visualmente a GameBoard
 
 
 #endif
