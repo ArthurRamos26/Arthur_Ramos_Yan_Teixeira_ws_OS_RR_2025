64edde75585ce541eef40956564c785e94bb67ca
Arthur Ramos <146880822+ArthurRamos26@users.noreply.github.com>
2025-06-01 20:03:07 -0400

    Update jogodavelha.h

--- Implementação do código/jogodavelha.h
+++ Implementação do código/jogodavelha.h
@@ -11,6 +11,13 @@ typedef struct {
 GameBoard* create_board();  // Função para criar o jogo de tabuleiro
 void free_board(GameBoard* board); //Liberar a memória apos o fim da execução do código 
 void display_board(const GameBoard* board); //Essa função cria visualmente a GameBoard
+bool is_valid_move(const GameBoard* board, int row, int col);//checa se o movimento é valido 
+bool make_move(GameBoard* board, int row, int col, char player);//faz o movimento 
+bool check_winner(const GameBoard* board, char player);//verifica o vencedor
+bool is_board_full(const GameBoard* board);//verifica se deu velha
+bool game_ended(const GameBoard* board);//termina o jogo 
+
+
 
 
 #endif
